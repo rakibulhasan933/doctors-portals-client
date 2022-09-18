bdffaf5fb08c660c80511eec8e0ea071df04c537
RAKIBUL <rakibulmd933@gmail.com>
2022-09-18 20:26:16 +0600

    [ADD] home completed

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import Banner from './Banner';
 import InfoCard from './InfoCard';
+import MakeAppointment from './MakeAppointment';
+import Services from './Services';
+import Testimonials from './Testimonials';
+import Treatment from './Treatment';
 
 const Home = () => {
 	return (
 		<div>
 			<Banner />
 			<InfoCard />
+			<Services />
+			<Treatment />
+			<MakeAppointment />
+			<Testimonials />
 		</div>
 	);
 };
