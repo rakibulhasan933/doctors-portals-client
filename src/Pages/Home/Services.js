4a525de54560d914cf2d780d6ad6cf5a95491515
RAKIBUL <rakibulmd933@gmail.com>
2022-09-18 20:04:41 +0600

    [ADD]services

---
+++ src/Pages/Home/Services.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import truth from '../../assets/images/whitening.png';
+import Cavity from '../../assets/images/cavity.png';
+import fluoride from '../../assets/images/fluoride.png';
+
+const Services = () => {
+	return (
+		<div className='my-9'>
+			<div className="py-5 text-center">
+				<h5 className='pb-3 text-xl font-medium text-teal-400 uppercase'>Ours Services</h5>
+				<h2 className='text-4xl'>Services We Provide</h2>
+			</div>
+			<div className="grid grid-cols-1 gap-5 mx-5 lg:grid-cols-3">
+				<div className="shadow-xl card w-96 bg-base-100">
+					<figure className="px-10 pt-10">
+						<img src={truth} alt="Shoes" className="rounded-xl" />
+					</figure>
+					<div className="items-center text-center card-body">
+						<h2 className="card-title">Shoes!</h2>
+						<p>If a dog chews shoes whose shoes does he choose?</p>
+					</div>
+				</div>
+				<div className="shadow-xl card w-96 bg-base-100">
+					<figure className="px-10 pt-10">
+						<img src={fluoride} alt="Shoes" className="rounded-xl" />
+					</figure>
+					<div className="items-center text-center card-body">
+						<h2 className="card-title">Shoes!</h2>
+						<p>If a dog chews shoes whose shoes does he choose?</p>
+					</div>
+				</div>
+				<div className="shadow-xl card w-96 bg-base-100">
+					<figure className="px-10 pt-10">
+						<img src={Cavity} alt="Shoes" className="rounded-xl" />
+					</figure>
+					<div className="items-center text-center card-body">
+						<h2 className="card-title">Shoes!</h2>
+						<p>If a dog chews shoes whose shoes does he choose?</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Services;
