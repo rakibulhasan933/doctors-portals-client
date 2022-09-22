78cd9aa064da4d1bf933534ab717c10702ac3979
RAKIBUL <rakibulmd933@gmail.com>
2022-09-22 13:09:46 +0600

    email handle

--- src/Pages/Login/ForgetPassword.js
+++ src/Pages/Login/ForgetPassword.js
@@ -1,20 +1,21 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useSendPasswordResetEmail } from 'react-firebase-hooks/auth';
+import { useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
 import auth from '../../firebase.init';
 import Loading from '../Shared/Loading';
 
 const ForgetPassword = () => {
 	const [sendPasswordResetEmail, sending] = useSendPasswordResetEmail(auth);
-	const [email, setEmail] = useState('');
+	const { register, handleSubmit, formState: { errors } } = useForm();
 
 	const navigate = useNavigate();
 
-	const sendPasswordReset = async () => {
-		await sendPasswordResetEmail(email)
+	const onSubmit = async (data) => {
+		await sendPasswordResetEmail(data?.email)
 		alert('send email');
 		navigate('/login');
-	};
+	}
 	if (sending) {
 		return <Loading />
 	};
@@ -24,18 +25,29 @@ const ForgetPassword = () => {
 			<div className="flex items-center justify-center w-2/4 shadow-xl card bg-base-100">
 				<div className="card-body">
 					<h2 className='text-2xl font-bold text-center'>Forget Password</h2>
-					<div className="mb-3 form-control">
-						<label className="label">
-							<span className="label-text">Your Email</span>
-						</label>
-						<label className="input-group">
-							<span>Email</span>
-							<input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="info@site.com" className="w-full input input-bordered" />
-						</label>
-					</div>
-					<div className="justify-center card-actions">
-						<button className="btn btn-primary" onClick={sendPasswordReset} >Sent Email </button>
-					</div>
+					<form onSubmit={handleSubmit(onSubmit)}>
+						<div className="w-full max-w-xs form-control">
+							<label className="label">
+								<span className="label-text">Email</span>
+							</label>
+							<input type="email" className="w-full max-w-xs input input-bordered" placeholder="info@site.com" {...register("email", {
+								required: {
+									value: true,
+									message: 'Email is Required'
+								},
+								pattern: {
+									value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+									message: 'Provide a valid Email'
+								}
+							})} />
+							<label className="label">
+								{errors.email?.type === 'required' && <span className="text-red-500 label-text-alt">{errors.email?.message}</span>}
+								{errors.email?.type === 'pattern' && <span className="text-red-500 label-text-alt">{errors.email?.message}</span>}
+							</label>
+
+						</div>
+						<input className='w-full max-w-xs text-white btn' type="submit" value="Sign Up" />
+					</form>
 				</div>
 			</div>
 		</div>
