6e1b530f82b1822b6d65d56826f77fbaae9d44fc
RAKIBUL <rakibulmd933@gmail.com>
2022-10-07 14:28:40 +0600

    available appointment

--- src/Pages/Appointment/Appointment.js
+++ src/Pages/Appointment/Appointment.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import AppointmentBanner from './AppointmentBanner';
+import AvailableAppointments from './AvailableAppointments';
 
 const Appointment = () => {
 	const [date, setDate] = useState(new Date());
 	return (
 		<div>
 			<AppointmentBanner date={date} setDate={setDate} />
+			<AvailableAppointments date={date} />
 		</div>
 	);
 };
---
+++ src/Pages/Appointment/AvailableAppointments.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AvailableAppointments = () => {
+	return (
+		<div>
+			<h1>AvailableAppointment</h1>
+		</div>
+	);
+};
+
+export default AvailableAppointments;
