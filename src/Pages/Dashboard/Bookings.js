10ba590385d107fa6cf1b330d0024297ec7061f2
RAKIBUL <rakibulmd933@gmail.com>
2022-10-18 00:08:26 +0600

    [ADD] booking added

--- src/Pages/Dashboard/Bookings.js
+++ src/Pages/Dashboard/Bookings.js
@@ -1,9 +1,45 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import Loading from '../Shared/Loading';
 
 const Bookings = () => {
+	const { data: bookings, isLoading } = useQuery(['bookings'], () => fetch('http://localhost:5000/bookings')
+		.then(res => res.json()))
+
+	if (isLoading) {
+		return <Loading />
+	}
 	return (
 		<div>
+			<div class="overflow-x-auto">
+				<table class="table w-full">
+					<thead>
+						<tr>
+							<th></th>
+							<th>Email</th>
+							<th>Name</th>
+							<th>Date</th>
+							<th>Time</th>
+							<th>Treatment</th>
+							<th>Payment</th>
+						</tr>
+					</thead>
+					<tbody>
+						{
+							bookings.map((a, index) => <tr key={a._id}>
+								<th>{index + 1}</th>
+								<td>{a.patient}</td>
+								<td>{a.patientName}</td>
+								<td>{a.date}</td>
+								<td>{a.slot}</td>
+								<td>{a.treatment}</td>
+							</tr>)
+						}
 
+
+					</tbody>
+				</table>
+			</div>
 		</div>
 	);
 };
