274b2af23bac784f8d26c10eaf760fad2e48474d
RAKIBUL <rakibulmd933@gmail.com>
2022-10-19 16:17:33 +0600

    [ADD] user amdin used

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link, Outlet } from 'react-router-dom';
+import auth from '../../firebase.init';
+import useAdmin from '../../hooks/useAdmin';
 
 const Dashboard = () => {
+	const [user] = useAuthState(auth);
+	const [admin] = useAdmin(user);
 	return (
 		<div className="drawer drawer-mobile">
 			<input id="my-dashboard" type="checkbox" className="drawer-toggle" />
@@ -14,8 +19,10 @@ const Dashboard = () => {
 					<li><Link to="/dashboard">My Appointments</Link></li>
 					<li><Link to="/dashboard/review">My Reviews</Link></li>
 					<li><Link to="/dashboard/history">My History</Link></li>
-					<li><Link to="/dashboard/bookings">ALL Booking</Link></li>
-					<li><Link to="/dashboard/users">ALL Users</Link></li>
+					{admin && <>
+						<li><Link to="/dashboard/bookings">ALL Booking</Link></li>
+						<li><Link to="/dashboard/users">ALL Users</Link></li>
+					</>}
 				</ul>
 
 			</div>
