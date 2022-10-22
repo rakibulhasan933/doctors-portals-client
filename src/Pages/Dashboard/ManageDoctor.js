da084d775c5b4f51e2a399de926454d007c8ab33
RAKIBUL <rakibulmd933@gmail.com>
2022-10-22 14:35:43 +0600

    [ADD] manage doctor display

--- src/Pages/Dashboard/ManageDoctor.js
+++ src/Pages/Dashboard/ManageDoctor.js
@@ -14,7 +14,49 @@ const ManageDoctor = () => {
 	console.log(doctors);
 	return (
 		<div>
-			<h1>Manage Doctor :{doctors.length}</h1>
+			<h1 className='text-2xl '>Manage Doctor :{doctors.length}</h1>
+			<div className="w-full overflow-x-auto">
+				<table className="table w-full">
+					<thead>
+						<tr>
+							<th>Name</th>
+							<th>Email</th>
+							<th>Specialty</th>
+							<th>Action</th>
+						</tr>
+					</thead>
+					<tbody>
+						{
+							doctors?.map(doctor =>
+								<tr>
+									<td>
+										<div className="flex items-center space-x-3">
+											<div className="avatar">
+												<div className="w-12 h-12 mask mask-squircle">
+													<img src={doctor.img} alt="Avatar Tailwind CSS Component" />
+												</div>
+											</div>
+											<div>
+												<div className="font-bold">{doctor.name}</div>
+												<div className="text-sm opacity-50">United States</div>
+											</div>
+										</div>
+									</td>
+									<td>
+										{doctor.email}
+										<br />
+										<span className="badge badge-ghost badge-sm">Desktop Support Technician</span>
+									</td>
+									<td>{doctor.specialty}</td>
+									<th>
+										<button className="btn btn-ghost btn-xs">details</button>
+									</th>
+								</tr>
+							)
+						}
+					</tbody>
+				</table>
+			</div>
 		</div>
 	);
 };
